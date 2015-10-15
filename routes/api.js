0063eb89717d61ac15f6c0ea2062cf1210778cd6
Sinisa Grubor <sinisa.grubor@gmail.com>
2015-10-15 17:01:13 +0200

    added domain whitelist + more detailed logging

--- routes/api.js
+++ routes/api.js
@@ -4,10 +4,18 @@ var router = express.Router();
 var phantom = require ('phantom');
 var fs = require('fs');
 
+var ALLOWED_ORIGINS = ['onefootball.com'];
+
 //Routes
 router.get('/screenshot/:url', function(req, res){
     var url = decodeURIComponent(req.params.url);
     console.log('Requested url - ' + url);
+    console.log('User agent: ' + req.headers['user-agent']);
+    console.log('Referrer: ' + req.headers['referrer']);
+    if(!isValidaOrigin(url)) {
+        res.status(403).send('Forbidden');
+        return;
+    }
     var startTime = new Date();
     phantom.create(function (ph) {
         ph.createPage(function (page) {
@@ -41,5 +49,24 @@ router.get('/screenshot/:url', function(req, res){
         });
     });
 });
+
+function isValidaOrigin(url) {
+    try{
+        var urlNoProtocol = url.split('//')[1];
+        var domain = urlNoProtocol.split('/')[0];
+        //domain should end with allowed origin
+        for(var i = 0; i < ALLOWED_ORIGINS.length; i++){
+            if(domain.indexOf(ALLOWED_ORIGINS[i], domain.length - ALLOWED_ORIGINS[i].length) !== -1) {
+                return true;
+            }
+        }
+        console.log('Origin not allowed' );
+        return false;
+    } catch (error) {
+        console.log('Invalid url passed');
+        console.log(error);
+        return false;
+    }
+}
 
 module.exports = router;
